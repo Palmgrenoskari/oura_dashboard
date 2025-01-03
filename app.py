a2c4559e0b6bd351d35a012a695b43e1d8c8aab4
Oskari Palmgren <jokpal@utu.fi>
2025-01-03 11:17:23 +0200

    Initial landing page

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Oura Dashboard", layout="wide")
+
+st.title("Oura Ring Dashboard")
+
+st.text("Beep boop...")
+
+st.radio("???", ["Cookie", "Trick", "Treat"])
