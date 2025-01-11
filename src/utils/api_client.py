1a6d9a59da8aa3227c4188a91f8c962d12e5e019
Oskari Palmgren <jokpal@utu.fi>
2025-01-11 12:44:05 +0200

    Apparently some issues with specifying start_date and end_date when calling the Oura API. Remove for now until resolved.

--- src/utils/api_client.py
+++ src/utils/api_client.py
@@ -9,27 +9,27 @@ def fetch_oura_data(api_token, endpoint, num_of_days=7):
     url = f"https://api.ouraring.com/v2/usercollection/{endpoint}"
     headers = {"Authorization": f"Bearer {api_token}"}
     
-    params = get_params(num_of_days)
-    response = requests.get(url, headers=headers, params=params)
+    # params = get_params(num_of_days)
+    response = requests.get(url, headers=headers)
     response.raise_for_status()  # Raise an error for failed requests
     return response.json()
 
-def get_params(num_of_days):
-    """
-    Get the start and end dates for a given number of days ago.
-    """
+# def get_params(num_of_days):
+#     """
+#     Get the start and end dates for a given number of days ago.
+#     """
     
-    from datetime import datetime, timedelta
-    yesterday = datetime.now().date() - timedelta(days=1)
+#     from datetime import datetime, timedelta
+#     yesterday = datetime.now().date() - timedelta(days=1)
     
-    if num_of_days == 1:
-        return {
-            "start_date": yesterday.strftime("%Y-%m-%d"),
-            "end_date": yesterday.strftime("%Y-%m-%d")
-        }
+#     if num_of_days == 1:
+#         return {
+#             "start_date": yesterday.strftime("%Y-%m-%d"),
+#             "end_date": yesterday.strftime("%Y-%m-%d")
+#         }
     
-    x_ago = yesterday - timedelta(days=num_of_days)
-    return {
-        "start_date": x_ago.strftime("%Y-%m-%d"),
-        "end_date": yesterday.strftime("%Y-%m-%d")
-    }
+#     x_ago = yesterday - timedelta(days=num_of_days)
+#     return {
+#         "start_date": x_ago.strftime("%Y-%m-%d"),
+#         "end_date": yesterday.strftime("%Y-%m-%d")
+#     }
