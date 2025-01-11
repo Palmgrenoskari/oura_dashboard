3e70fe95dfc7cfafcbf484a11badda9be30266b9
Oskari Palmgren <jokpal@utu.fi>
2025-01-11 13:57:08 +0200

    Add a different handling of start and end_date for certain endpoints such as sleep where today actually means last night. Compared to for example activity where today actually means today's activity.

--- src/utils/api_client.py
+++ src/utils/api_client.py
@@ -20,22 +20,30 @@ def fetch_oura_data(api_token, endpoint, num_of_days=7):
     url = f"https://api.ouraring.com/v2/usercollection/{endpoint}"
     headers = {"Authorization": f"Bearer {api_token}"}
     
-    params = get_params(num_of_days)
+    params = get_params(endpoint, num_of_days)
     response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()  # Raise an error for failed requests
     return response.json()
 
-def get_params(num_of_days):
+def get_params(endpoint, num_of_days):
     """
     Get the start and end dates for a given number of days ago.
     Excludes today to avoid incomplete data.
+    Except for certain endpoints such as sleep where today basically means last night.
     """
-    
     from datetime import datetime, timedelta
-    yesterday = datetime.now().date() - timedelta(days=1)
-    x_ago = yesterday - timedelta(days=num_of_days)
     
-    return {
-        "start_date": x_ago.strftime("%Y-%m-%d"),
-        "end_date": yesterday.strftime("%Y-%m-%d")
-    }
+    if endpoint == "sleep":
+        today = datetime.now().date()
+        x_ago = today - timedelta(days=num_of_days)
+        return {
+            "start_date": x_ago.strftime("%Y-%m-%d"),
+            "end_date": today.strftime("%Y-%m-%d")
+        }
+    else:
+        yesterday = datetime.now().date() - timedelta(days=1)
+        x_ago = yesterday - timedelta(days=num_of_days)
+        return {
+            "start_date": x_ago.strftime("%Y-%m-%d"),
+            "end_date": yesterday.strftime("%Y-%m-%d")
+        }
