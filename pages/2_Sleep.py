c74ba43bdafd3ca5dc1933dfd6e39ae515e90852
Oskari Palmgren <jokpal@utu.fi>
2025-01-11 13:56:08 +0200

    Add a basic dayrange selector for over time graphs

--- pages/2_Sleep.py
+++ pages/2_Sleep.py
@@ -10,7 +10,20 @@ st.title("Sleep Analysis 😴")
 api_key = st.session_state.get('api_key', None)
 
 if api_key:
-    sleep_data = process_sleep_data(api_key)
-    display_sleep_charts(sleep_data)
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        # Add date range selector
+        date_range = st.select_slider(
+            "Select the number of days to display",
+            options=range(7, 31),
+            value=7,
+            help="Choose how many days of historical sleep data to display"
+        )
+    
+    # Add loading state while fetching data
+    with st.spinner(f'Fetching your last {date_range} days of sleep data...'):
+        sleep_data = process_sleep_data(api_key, date_range)
+        display_sleep_charts(sleep_data)
 else:
     st.info("Please go back to 'Home' page to connect your Oura Ring.")
