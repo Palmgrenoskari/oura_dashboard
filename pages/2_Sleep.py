853a8a55b76d2d6d655f4a438455fa0c1282d8c7
Oskari Palmgren <jokpal@utu.fi>
2025-01-13 13:47:50 +0200

    Remove try except, add st.stop with info message guiding user.

--- pages/2_Sleep.py
+++ pages/2_Sleep.py
@@ -21,29 +21,35 @@ if api_key:
             max_value=datetime.now().date(),
             help="Select the day to view daily sleep data for, default = last night"
         )
-    try:
-        with col2:
-        # Add date range selector
-        
-            start_date, end_date = st.date_input(
-                "Select date range",
-                value=(datetime.now().date().replace(day=1), datetime.now().date()),
-                min_value=datetime(2020, 1, 1).date(),
-                max_value=datetime.now().date(),
-                help="Choose the date range to display sleep data for, default = last 7 days"
-            )
-            
-            # Ensure we have at least the last 7 days of data from today for the averages
-            data_start_date = min(selected_day, start_date, (datetime.now().date() - timedelta(days=6)))
-            # day count
-            day_count = (datetime.now().date() - data_start_date).days + 1
-        
+    with col2:
+    # Add date range selector
     
-        # Add loading state while fetching data
-        with st.spinner(f'Please wait, fetching your sleep data...'):
-            sleep_data = process_sleep_data(api_key, day_count)
-            display_sleep_charts(sleep_data, selected_day, start_date, end_date)
-    except Exception as e:
-        st.info("Please select a valid date range")
+        date_range = st.date_input(
+            "Select date range",
+            value=(datetime.now().date().replace(day=1), datetime.now().date()),
+            min_value=datetime(2020, 1, 1).date(),
+            max_value=datetime.now().date(),
+            help="Choose the date range to display sleep data for, default = last 7 days"
+        )
+        
+        # Check if both dates are selected
+        if len(date_range) != 2:
+            _, col2b = st.columns(2)
+            with col2b:
+                st.info("Please select both a start and end date")
+            st.stop()
+            
+        start_date, end_date = date_range
+        
+        # Ensure we have at least the last 7 days of data from today for the averages
+        data_start_date = min(selected_day, start_date, (datetime.now().date() - timedelta(days=6)))
+        # day count
+        day_count = (datetime.now().date() - data_start_date).days + 1
+    
+
+    # Add loading state while fetching data
+    with st.spinner(f'Please wait, fetching your sleep data...'):
+        sleep_data = process_sleep_data(api_key, day_count)
+        display_sleep_charts(sleep_data, selected_day, start_date, end_date)
 else:
     st.info("Please go back to 'Home' page to connect your Oura Ring.")
