0348ece62438ddb819b1da22d3de25e7d191b05f
Oskari Palmgren <jokpal@utu.fi>
2025-01-13 12:44:33 +0200

    Fix error handling when selecting date range

--- pages/2_Sleep.py
+++ pages/2_Sleep.py
@@ -21,28 +21,29 @@ if api_key:
             max_value=datetime.now().date(),
             help="Select the day to view daily sleep data for, default = last night"
         )
-    
-    with col2:
+    try:
+        with col2:
         # Add date range selector
-        try:
+        
             start_date, end_date = st.date_input(
                 "Select date range",
                 value=(datetime.now().date().replace(day=1), datetime.now().date()),
                 min_value=datetime(2020, 1, 1).date(),
                 max_value=datetime.now().date(),
-                help="Choose the date range to display sleep data for"
+                help="Choose the date range to display sleep data for, default = last 7 days"
             )
             
             # Ensure we have at least the last 7 days of data from today for the averages
             data_start_date = min(selected_day, start_date, (datetime.now().date() - timedelta(days=6)))
             # day count
             day_count = (datetime.now().date() - data_start_date).days + 1
-        except Exception as e:
-            st.info("Please select a valid date range")
+        
     
-    # Add loading state while fetching data
-    with st.spinner(f'Fetching your sleep data...'):
-        sleep_data = process_sleep_data(api_key, day_count)
-        display_sleep_charts(sleep_data, selected_day, start_date, end_date)
+        # Add loading state while fetching data
+        with st.spinner(f'Please wait, fetching your sleep data...'):
+            sleep_data = process_sleep_data(api_key, day_count)
+            display_sleep_charts(sleep_data, selected_day, start_date, end_date)
+    except Exception as e:
+        st.info("Please select a valid date range")
 else:
     st.info("Please go back to 'Home' page to connect your Oura Ring.")
