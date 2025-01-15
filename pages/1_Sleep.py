26c87930c48b4ef14e96ac3c5b4ca97ed7f90ecd
Oskari Palmgren <jokpal@utu.fi>
2025-01-15 16:05:39 +0200

    Add demo data functionality to sleep

--- pages/1_Sleep.py
+++ pages/1_Sleep.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from src.utils.data_processing import process_sleep_data
+from src.utils.data_processing import process_sleep_data, load_sleep_data
 from src.components.sleep_visualizations import display_sleep_charts
 from datetime import datetime, timedelta
 
@@ -52,4 +52,39 @@ if api_key:
         sleep_data = process_sleep_data(api_key, day_count)
         display_sleep_charts(sleep_data, selected_day, start_date, end_date)
 else:
-    st.info("Please go back to 'Home' page to connect your Oura Ring.")
+    st.info("Note! Using demo data. Data is only available for days between 2025-01-02 and 2025-01-15")
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        selected_day = st.date_input(
+            "Select day", value=datetime(2025, 1, 15).date(),
+            min_value=datetime(2025, 1, 2).date(),
+            max_value=datetime(2025, 1, 15).date(),
+            help="Select the day to view daily sleep data for, default = Jan 15th 2025"
+        )
+    with col2:
+    # Add date range selector
+    
+        date_range = st.date_input(
+            "Select date range",
+            value=(datetime(2025, 1, 2).date(), datetime(2025, 1, 15).date()),
+            min_value=datetime(2025, 1, 2).date(),
+            max_value=datetime(2025, 1, 15).date(),
+            help="Choose the date range to display sleep data for, default = Jan 2nd 2025 - Jan 15th 2025"
+        )
+        
+        # Check if both dates are selected
+        if len(date_range) != 2:
+            _, col2b = st.columns(2)
+            with col2b:
+                st.info("Please select both a start and end date")
+            st.stop()
+            
+        start_date, end_date = date_range
+        
+    
+
+    # Add loading state while fetching data
+    with st.spinner(f'Please wait, fetching your sleep data...'):
+        sleep_data = load_sleep_data()
+        display_sleep_charts(sleep_data, selected_day, start_date, end_date)
